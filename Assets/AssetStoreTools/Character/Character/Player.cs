e70d1651f995b2856abdd11e9f89dad4cbcadf52
LeeHann <0104ihn@naver.com>
2022-09-28 21:36:52 +0900

    Hexmap Editor import

--- Assets/AssetStoreTools/Character/Character/Player.cs
+++ Assets/AssetStoreTools/Character/Character/Player.cs
@@ -16,20 +16,20 @@ public class Player : MonoBehaviour {
 			anim = gameObject.GetComponentInChildren<Animator>();
 		}
 
-		void Update (){
-			if (Input.GetKey ("w")) {
-				anim.SetInteger ("AnimationPar", 1);
-			}  else {
-				anim.SetInteger ("AnimationPar", 0);
-			}
+		// void Update (){
+		// 	if (Input.GetKey ("w")) {
+		// 		anim.SetInteger ("AnimationPar", 1);
+		// 	}  else {
+		// 		anim.SetInteger ("AnimationPar", 0);
+		// 	}
 
-			if(controller.isGrounded){
-				moveDirection = transform.forward * Input.GetAxis("Vertical") * speed;
-			}
+		// 	if(controller.isGrounded){
+		// 		moveDirection = transform.forward * Input.GetAxis("Vertical") * speed;
+		// 	}
 
-			float turn = Input.GetAxis("Horizontal");
-			transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
-			controller.Move(moveDirection * Time.deltaTime);
-			moveDirection.y -= gravity * Time.deltaTime;
-		}
+		// 	float turn = Input.GetAxis("Horizontal");
+		// 	transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
+		// 	controller.Move(moveDirection * Time.deltaTime);
+		// 	moveDirection.y -= gravity * Time.deltaTime;
+		// }
 }
