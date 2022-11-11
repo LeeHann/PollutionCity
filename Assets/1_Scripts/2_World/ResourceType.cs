111dc4dec3808d9d411f2c1ed73820b38867305b
koskim1 <koskim1@naver.com>
2022-11-11 22:32:05 +0900

    feat: 탐사 유닛 자원 획득 시 한글 안내 메시지

--- Assets/1_Scripts/2_World/ResourceType.cs
+++ Assets/1_Scripts/2_World/ResourceType.cs
@@ -6,3 +6,35 @@ public enum ResourceType {
     Glass,
     Plastic
 }
+
+public static class ReosourcTypeFormatter {
+    public static string Rsc2Str(this ResourceType val) {
+        string ret = "";
+        switch (val)
+        {
+            case ResourceType.None:
+                break;
+            
+            case ResourceType.Money:
+                ret = "돈";
+                break;
+            
+            case ResourceType.Paper:
+                ret = "종이류";
+                break;
+            
+            case ResourceType.Can:
+                ret = "캔류";
+                break;
+            
+            case ResourceType.Glass:
+                ret = "유리류";
+                break;
+            
+            case ResourceType.Plastic:
+                ret = "플라스틱류";
+                break;
+        }
+        return ret;
+    }
+}
