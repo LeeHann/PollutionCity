81ba7e29583513edec2fbf719482f1be277c50d7
LeeHann <0104ihn@naver.com>
2022-11-28 21:35:48 +0900

    chore: mlagent 패키지 추가

---
+++ Assets/1_Scripts/2_World/AI/CityAgent.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
+
+public class CityAgent : Agent
+{
+    
+}
