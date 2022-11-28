0ee43695be08d65c7bcd2842a54b07f67095ad85
LeeHann <0104ihn@naver.com>
2022-11-29 00:46:25 +0900

    fix: agentEnv 세팅중

--- Assets/1_Scripts/2_World/AI/CityAgent.cs
+++ Assets/1_Scripts/2_World/AI/CityAgent.cs
@@ -7,5 +7,28 @@ using Unity.MLAgents.Actuators;
 
 public class CityAgent : Agent
 {
-    
+    private void Start() 
+	{
+		
+	}
+
+	public override void OnEpisodeBegin()
+	{
+
+	}
+
+	public override void CollectObservations(VectorSensor sensor)
+	{
+
+	}
+
+	public override void OnActionReceived(ActionBuffers actions)
+	{
+
+	}
+
+	public override void Heuristic(in ActionBuffers actionsOut)
+	{
+		
+	}
 }
